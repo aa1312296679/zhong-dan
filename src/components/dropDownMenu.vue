55da83b3791e53abfb6e1492cabb50d5fdc6f94b
aa1312296679 <1312296679@qq.com>
2019-12-05 01:48:43 +0800

    已完成旋转角度的动画

--- src/components/dropDownMenu.vue
+++ src/components/dropDownMenu.vue
@@ -15,43 +15,64 @@
             <div class="drop_down_item">列表项</div>
             <div class="drop_down_item">列表项</div>
         </div>
-        <div class="drop_down_ico_wrapper">
-        </div>
+        <div :class="['drop_down_ico_wrapper',icoActive,'iconfont']" @click="icoHandle"></div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "dropDownMenu"
+        name: "dropDownMenu",
+        data(){
+            return {
+                isRotate:false //旋转状态
+            }
+        },
+        computed:{
+            icoActive(){
+                return this.isRotate?'drop_down_ico_active':''
+            }
+        },
+        methods:{
+            icoHandle(){
+                this.isRotate=!this.isRotate;
+            }
+        }
     }
 </script>
 
 <style lang="stylus" scoped>
 .drop_down_menu
-    border 1px dashed red
     position fixed
-    right: 0;
+    right: 20px
     top 140px
     .drop_down_content,.drop_down_ico_wrapper
         float left
-    .drop_down_ico_wrapper::before,.drop_down_content .drop_down_item
+    .drop_down_ico_wrapper,.drop_down_content .drop_down_item
         background rgba(12, 32, 53, 0.8)
         border-radius 2px
-    .drop_down_ico_wrapper::before
-        content ""
-        display block
-        width 12px
-        height 29.5px
-        border 1px solid red
-        margin-top 5px
+        color #ffff
+    .drop_down_ico_wrapper
+        margin-top 4.2px
+        cursor: pointer
+    .drop_down_ico_wrapper,.drop_down_ico_active
+        &::before
+            content "\e604"
+            display block
+            width 12px
+            height 29.5px
+            text-indent: -2px
+            font-weight bold
+            line-height: 29.5px
+            transition transform 0.5s ease
+    .drop_down_ico_active
+        &::before
+            transform:rotate(180deg)
     .drop_down_content
-        border 1px dashed blue
         margin-right 6px
         .drop_down_item
             width 246.45px
             overflow hidden
             padding 3.5px 0
-            border 1px dashed greenyellow
             border-radius 2px
             margin-top 4px
             color #fff
@@ -59,7 +80,6 @@
                 height 23px
             .drop_down_item_logo
                 width 23px
-                border 1px solid red
                 display block
                 float left
                 margin-left 20px
@@ -67,5 +87,4 @@
             .text
                 float left
                 display block
-                border 1px solid orange
 </style>
